284703d19992f6997d4f3eba6dda55f6d2de11e1
Peter Dinis <peterdinis0@pobox.sk>
2024-12-08 17:00:50 +0100

    feat: Adding env here

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
-"github.com/gofiber/fiber/v2/middleware/cors"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"os"
+	"log"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	app := fiber.New()
+
 	app.Use(cors.New())
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	appPort := os.Getenv("PORT")
+	if appPort == "" {
+		appPort = ":5000"
+	} else if appPort[0] != ':' {
+		appPort = ":" + appPort
+	}
+
+	err = app.Listen(appPort)
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
 }
