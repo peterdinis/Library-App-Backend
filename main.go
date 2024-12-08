7e525b0b3a1b68bddbbeea0442733faa9a0a23e5
Peter Dinis <peterdinis0@pobox.sk>
2024-12-08 14:13:13 +0100

    First endpoint done

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/gofiber/fiber/v2"
+"github.com/gofiber/fiber/v2/middleware/cors"
 
 func main() {
-    fmt.Println("Hello, World!")
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
 }
